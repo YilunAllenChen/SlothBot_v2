99316c4cdde58a1f72017cfab18f8c0a0258b8c6
YilunAllenChen <allenchen@gatech.edu>
2021-06-05 12:41:58 -0400

    exception handling and logging for cloud comm

--- bin_RPi/programs/cloud_comm.py
+++ bin_RPi/programs/cloud_comm.py
@@ -6,34 +6,45 @@ from firebase_admin import firestore
 from random import choice, gauss, random
 from time import time, sleep
 from requests import get
-
 import uuid
+import pathlib
+import logging
+pathlib.Path("./logs").mkdir(parents=True, exist_ok=True)
+logger = logging.getLogger("cloud")
+logger.setLevel("INFO")
+f_handler = logging.FileHandler("logs/cloud.log", "a+")
+f_handler.setFormatter(logging.Formatter("[%(asctime)s] [%(levelname)s] %(message)s"))
+logger.addHandler(f_handler)
+
 
 # Use a service account
-cred = credentials.Certificate('agent_cred.json.secret')
-firebase_admin.initialize_app(cred)
+try:
+    cred = credentials.Certificate('agent_cred.json.secret')
+    firebase_admin.initialize_app(cred)
 
-db = firestore.client()
+    db = firestore.client()
 
-datatypes = [u'temperature_C', u'temperature_F',
-             u"battery_voltage", u"humidity"]
-
-
-
+    datatypes = [u'temperature_C', u'temperature_F',
+                u"battery_voltage", u"humidity"]
+except Exception as e:
+    logger.error(str(e))
 
 while(True):
-    doc_ref = db.collection(u'sensor_data').document(f"AGENT_{hex(uuid.getnode())}")
-    timestamp = str(int(time() * 1000 + random()*10000-5000))
-    doc_ref.set({
-        "env_data": {
-            timestamp: {
-                u'type': choice(datatypes),
-                u'data': (gauss(10, 2)),
+    try:
+        doc_ref = db.collection(u'sensor_data').document(f"AGENT_{hex(uuid.getnode())}")
+        timestamp = str(int(time() * 1000 + random()*10000-5000))
+        doc_ref.set({
+            "env_data": {
+                timestamp: {
+                    u'type': choice(datatypes),
+                    u'data': (gauss(10, 2)),
+                }
+            },
+            "identity": {
+                "ip_addr": get('https://api.ipify.org').text
             }
-        },
-        "identity": {
-            "ip_addr": get('https://api.ipify.org').text
-        }
-    }, merge=True)
-    print(f"sent")
-    sleep(5)
+        }, merge=True)
+        print(f"sent")
+        sleep(5)
+    except Exception as e:
+        logger.error(str(e))
