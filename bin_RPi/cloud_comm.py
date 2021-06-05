8467494d898abfcde3a3fd5e3604b98029e09351
YilunAllenChen <allenchen@gatech.edu>
2021-06-05 12:30:11 -0400

    added cloud comm and print ip addr

--- bin_RPi/cloud_comm.py
+++ bin_RPi/cloud_comm.py
@@ -4,7 +4,10 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 from random import choice, gauss, random
-from time import time
+from time import time, sleep
+from requests import get
+
+import uuid
 
 # Use a service account
 cred = credentials.Certificate('agent_cred.json.secret')
@@ -14,12 +17,23 @@ db = firestore.client()
 
 datatypes = [u'temperature_C', u'temperature_F',
              u"battery_voltage", u"humidity"]
-for i in range(30):
-    doc_ref = db.collection(u'sensor_data').document(str(i))
+
+
+
+
+while(True):
+    doc_ref = db.collection(u'sensor_data').document(f"AGENT_{hex(uuid.getnode())}")
+    timestamp = str(int(time() * 1000 + random()*10000-5000))
     doc_ref.set({
-        str(int(time() * 1000 + random()*10000-5000)): {
-            u'type': choice(datatypes),
-            u'data': (gauss(10, 2)),
+        "env_data": {
+            timestamp: {
+                u'type': choice(datatypes),
+                u'data': (gauss(10, 2)),
+            }
+        },
+        "identity": {
+            "ip_addr": get('https://api.ipify.org').text
         }
     }, merge=True)
     print(f"sent")
+    sleep(5)
