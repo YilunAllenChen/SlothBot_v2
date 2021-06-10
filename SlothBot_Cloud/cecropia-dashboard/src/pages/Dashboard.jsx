69e113de3cf749298de2b44449e220e8024ec1cd
YilunAllenChen <allenchen@gatech.edu>
2021-06-10 15:05:46 -0400

    dashbaord page

--- SlothBot_Cloud/cecropia-dashboard/src/pages/Dashboard.jsx
+++ SlothBot_Cloud/cecropia-dashboard/src/pages/Dashboard.jsx
@@ -1,37 +1,46 @@
 import React from "react";
 import {
-  Card,
   Col,
   Container,
   Row,
-  CardHeader,
-  CardBody,
-  CardTitle,
-  Button
+  Button,
+  ListGroup,
+  ListGroupItem,
+  Badge,
 } from "shards-react";
 
-import DB from "../apis/database"
-import { useState } from "react"
-import { useDispatch } from "react-redux"
-import { toPage, selectActiveAgentAndUnreadyData} from "../store/store"
+import DB from "../apis/database";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { toPage, selectActiveAgentAndUnreadyData } from "../store/store";
 
 const cardStyle = {
   margin: "10px",
 };
 
 export default function BasicCardExample() {
-
   const dispatch = useDispatch();
 
-  function handleConnect(agent_id){
-    
+  let [dataReady, setDataReady] = useState(false);
+  let [data, setData] = useState([]);
+
+  function handleConnect(agent_id) {
     dispatch(selectActiveAgentAndUnreadyData(agent_id));
     dispatch(toPage("controller"));
   }
 
-
-  let [dataReady, setDataReady] = useState(false);
-  let [data, setData] = useState([]);
+  const handleRefreshListOfAgents = () => {
+    console.log("refreshing");
+    setDataReady(false);
+    DB.get().then((data) => {
+      let all_agents = [];
+      data.docs.forEach((doc) => {
+        all_agents.push(doc.data());
+      });
+      setData(all_agents);
+      setDataReady(true);
+    });
+  };
 
   if (!dataReady) {
     DB.get().then((data) => {
@@ -49,35 +58,88 @@ export default function BasicCardExample() {
     );
   }
 
-  let all_cards = []
-  for(let agent of data){
-
+  let all_cards = [];
+  for (let agent of data) {
     let online;
-    if (agent.state.heartbeat > (Date.now() - 300 * 1000)){
-      online = (<span style={{color: "#00AA00"}}>ONLINE</span>);
+    let button;
+    if (agent.state.heartbeat > Date.now() - 300 * 1000) {
+      online = (
+        <Badge outline theme="success">
+          ONLINE
+        </Badge>
+      );
+      button = <Button onClick={() => handleConnect(agent.id)}>Control</Button>;
     } else {
-      online = (<span style={{color: "#AAAAAA"}}>OFFLINE</span>);
+      online = (
+        <Badge outline theme="light">
+          OFFLINE
+        </Badge>
+      );
+      button = (
+        <Button
+          outline
+          theme="dark"
+          onClick={() => handleConnect(agent.id)}
+          disabled
+        >
+          Control
+        </Button>
+      );
     }
 
     all_cards.push(
-        <Col sm="12" md="4">
-          <Card className="black" style={cardStyle}>
-            <CardHeader>{agent.id}</CardHeader>
-            <CardBody>
-              <CardTitle>{online}</CardTitle>
-              <Button onClick={()=>handleConnect(agent.id)}>Connect</Button>
-            </CardBody>
-          </Card>
-        </Col>
-    )
+      <ListGroupItem>
+        <Row className="black">
+          <Col xs="12" md="6" lg="9">
+            {agent.id}
+          </Col>
+          <Col xs="6" md="3" lg="1">
+            <Row style={{ top: "25%", position: "absolute" }}>
+              <Col>{online}</Col>
+            </Row>
+          </Col>
+          <Col xs="6" md="3" lg="2">
+            {button}
+          </Col>
+        </Row>
+        {/* <ListGroupItemHeading>{agent.id}</ListGroupItemHeading> */}
+        {/* <ListGroupItemText>{online}</ListGroupItemText> */}
+      </ListGroupItem>
+      // <Col sm="12" md="4">
+      //   <Card className="black" style={cardStyle}>
+      //     <CardHeader>{agent.id}</CardHeader>
+      //     <CardBody>
+      //       <CardTitle>{online}</CardTitle>
+      //       <Button onClick={()=>handleConnect(agent.id)}>Connect</Button>
+      //     </CardBody>
+      //   </Card>
+      // </Col>
+    );
   }
-
-
 
   return (
     <Container>
+      <hr />
       <Row>
-        {all_cards}
+        <Col xs="12" md="10">
+          <h1 className="white">The SlothBot Network</h1>
+        </Col>
+        <Col xs='12' md='2'>
+                    <Button
+            theme="info"
+            // style={{ width: "100%" }}
+            onClick={handleRefreshListOfAgents}
+          >
+            Refresh
+          </Button>
+        </Col>
+      </Row>
+      <hr />
+      <Row>
+      </Row>
+      <Row>
+        <ListGroup style={{ width: "100%" }}>{all_cards}</ListGroup>
+        {/* {all_cards} */}
       </Row>
     </Container>
   );
